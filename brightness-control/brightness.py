c6a6b0a05062cc9bcef828baef3fd3c606f8c3c3
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2013-06-23 19:24:39 -0400

    Ported to use PyGtk3

--- brightness-control/brightness.py
+++ brightness-control/brightness.py
@@ -1,31 +1,61 @@
 #!/usr/bin/env python
 
-import gtk
+from gi.repository import Gtk
 import subprocess
 
 class BrightnessScale:
     def __init__(self):
-        window = gtk.Window()
+        self.monitor = self.getActiveMonitor()
+        self.currB = self.getCurrentBrightness()
+        
+    def initUI(self):
+        window = Gtk.Window()
         window.set_title('Brightness Scale')
         window.set_default_size(250, 50)
-        #Find display monitor
-        self.monitor = subprocess.check_output("xrandr -q | grep ' connected' | cut -d ' ' -f1", shell=True)
-        self.monitor = self.monitor.strip().rstrip('\n')
-
-        #Find current brightness
-        currBrightness = subprocess.check_output("xrandr --verbose | grep -i brightness | cut -f2 -d:", shell=True)
-        currBrightness = int(float(currBrightness) * 100)
+        window.set_position(Gtk.WindowPosition.CENTER)
         
-        adjustment = gtk.Adjustment(currBrightness, 0, 100, 1, 10, 0)
-        self.scale = gtk.HScale(adjustment)
+        self.adjustment = Gtk.Adjustment(self.currB, 0, 100, 1, 10, 0)
+        self.scale = Gtk.HScale()
+        self.scale.set_adjustment(self.adjustment)
         self.scale.set_digits(0)
-        self.scale.set_update_policy(gtk.UPDATE_DELAYED)
         
-        window.connect("destroy", lambda w: gtk.main_quit())
+        window.connect("destroy", lambda w: Gtk.main_quit())
         self.scale.connect("value-changed", self.scale_moved)
         
+        window.set_border_width(10)
         window.add(self.scale)
         window.show_all()
+        
+    def showErrDialog(self):
+        self.errDialog = Gtk.MessageDialog(None, 
+                                           Gtk.DialogFlags.MODAL,
+                                           Gtk.MessageType.ERROR,
+                                           Gtk.ButtonsType.OK,
+                                           "Unable to detect active monitor, run 'xrandr --verbose' on command-line for more info")
+        self.errDialog.run()
+        self.errDialog.destroy()
+
+    def initStatus(self):
+        if(self.monitor == "" or self.currB == ""):
+            return False
+        return True
+        
+    def getActiveMonitor(self):
+        #Find display monitor
+        monitor = subprocess.check_output("xrandr -q | grep ' connected' | cut -d ' ' -f1", shell=True)
+        if(monitor != ""):
+            monitor = monitor.split('\n')[0]
+        return monitor
+
+    def getCurrentBrightness(self):
+        #Find current brightness
+        currB = subprocess.check_output("xrandr --verbose | grep -i brightness | cut -f2 -d:", shell=True)
+        if(currB != ""):
+            currB = currB.split('\n')[0]
+            currB = int(float(currB) * 100)
+        else:
+            currB = ""
+        return currB
 
     def scale_moved(self, event):
         #Change brightness
@@ -33,5 +63,9 @@ class BrightnessScale:
         cmd = "xrandr --output %s --brightness %.2f" % (self.monitor, newBrightness)
         cmdStatus = subprocess.check_output(cmd, shell=True)
 
-BrightnessScale()
-gtk.main()
+brcontrol = BrightnessScale()
+if(brcontrol.initStatus()):
+    brcontrol.initUI()
+    Gtk.main()
+else:
+    brcontrol.showErrDialog()
