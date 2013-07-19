5c1d46d75653fb96c294236e6debb2d44d08dc3a
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2013-07-18 20:56:19 -0400

    Added comments to file

--- brightness-control/brightness.py
+++ brightness-control/brightness.py
@@ -5,28 +5,37 @@ import subprocess
 
 class BrightnessScale:
     def __init__(self):
+        # get active monitor and current brightness
         self.monitor = self.getActiveMonitor()
         self.currB = self.getCurrentBrightness()
         
     def initUI(self):
+        # initliaze and configure window 
         window = Gtk.Window()
         window.set_title('Brightness Scale')
         window.set_default_size(250, 50)
         window.set_position(Gtk.WindowPosition.CENTER)
+        window.set_border_width(10)
         
+        # slider configuration
         self.adjustment = Gtk.Adjustment(self.currB, 0, 100, 1, 10, 0)
         self.scale = Gtk.HScale()
         self.scale.set_adjustment(self.adjustment)
         self.scale.set_digits(0)
         
+        # close Gtk thread on closing window
         window.connect("destroy", lambda w: Gtk.main_quit())
+
+        # setup event handler on value-changed
         self.scale.connect("value-changed", self.scale_moved)
         
-        window.set_border_width(10)
+        # add the scale to window
         window.add(self.scale)
+
+        # show all components in window
         window.show_all()
         
-        #Close on Escape
+        # close window on pressing escape key
         accGroup = Gtk.AccelGroup()
         key, modifier = Gtk.accelerator_parse('Escape')
         accGroup.connect(key, modifier, Gtk.AccelFlags.VISIBLE, Gtk.main_quit)
@@ -38,6 +47,7 @@ class BrightnessScale:
                                            Gtk.MessageType.ERROR,
                                            Gtk.ButtonsType.OK,
                                            "Unable to detect active monitor, run 'xrandr --verbose' on command-line for more info")
+        self.errDialog.set_title("brightness control error")
         self.errDialog.run()
         self.errDialog.destroy()
 
@@ -55,7 +65,7 @@ class BrightnessScale:
 
     def getCurrentBrightness(self):
         #Find current brightness
-        currB = subprocess.check_output("xrandr --verbose | grep -i brightness | cut -f2 -d:", shell=True)
+        currB = subprocess.check_output("xrandr --verbose | grep -i brightness | cut -f2 -d ' '", shell=True)
         if(currB != ""):
             currB = currB.split('\n')[0]
             currB = int(float(currB) * 100)
@@ -69,9 +79,13 @@ class BrightnessScale:
         cmd = "xrandr --output %s --brightness %.2f" % (self.monitor, newBrightness)
         cmdStatus = subprocess.check_output(cmd, shell=True)
 
-brcontrol = BrightnessScale()
-if(brcontrol.initStatus()):
-    brcontrol.initUI()
-    Gtk.main()
-else:
-    brcontrol.showErrDialog()
+if __name__ == "__main__":
+    # new instance of BrightnessScale
+    brcontrol = BrightnessScale()
+    if(brcontrol.initStatus()):
+        # if everything ok, invoke UI and start Gtk thread loop
+        brcontrol.initUI()
+        Gtk.main()
+    else:
+        # show error dialog
+        brcontrol.showErrDialog()
